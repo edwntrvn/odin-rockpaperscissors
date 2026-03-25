4ba0cb50a63478d1f6490ff04ed6b8453e1a52f5
edwntrvn <97424504+edwntrvn@users.noreply.github.com>
2026-03-25 12:13:06 -0400

    Create function to get user input

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 // Write the logic to get the computer choice.
 // Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
 
-
-
 // Create a function that generates a random number between 1, 2, and 3 when executed
 // If the function outputs 1, return "Rock"
 // Else if the function outputs 2, return "Paper"
@@ -11,4 +9,17 @@
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3 + 1);
   return computerChoice;
+}
+
+
+
+// Write the logic to get the human choice.
+// Your game will be played by a human player. You will write a function that takes the user choice and returns it.
+
+// Create a function that asks the user to input a number between 1, 2, and 3 when executed
+// Return the user's input
+
+function getHumanChoice() {
+  let humanChoice = prompt("Choose a number between 1, 2, and 3");
+  return parseInt(humanChoice);
 }
