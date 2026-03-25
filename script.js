ed0b79db72386bdf1f6eeea15d87d469d11ba39b
edwntrvn <97424504+edwntrvn@users.noreply.github.com>
2026-03-25 12:33:57 -0400

    Edit function to get computer input

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 // Write the logic to get the computer choice.
-// Your game will be played against the computer. You will write a function that randomly returns “rock”, “paper” or “scissors”.
+// Your game will be played against the computer. You will write a function that randomly returns 
+// “rock”, “paper” or “scissors”.
 
 // Create a function that generates a random number between 1, 2, and 3 when executed
-// If the function outputs 1, return "Rock"
-// Else if the function outputs 2, return "Paper"
-// Else if the function outputs 3, return "Scissors"
+// If the function outputs 1, return "rock"
+// Else if the function outputs 2, return "paper"
+// Else if the function outputs 3, return "scissors"
 
 function getComputerChoice() {
   let computerChoice = Math.floor(Math.random() * 3 + 1);
-  return computerChoice;
+
+  if (computerChoice === 1) {
+    return "rock";
+  }
+
+  else if (computerChoice === 2) {
+    return "paper";
+  }
+
+  else {
+    return "scissors";
+  }
 }
 
 
@@ -17,6 +29,9 @@ function getComputerChoice() {
 // Your game will be played by a human player. You will write a function that takes the user choice and returns it.
 
 // Create a function that asks the user to input a number between 1, 2, and 3 when executed
+// If the user inputs 1, return "Rock"
+// Else if the user inputs 2, return "Paper"
+// Else if the user inputs 3, return "Scissors"
 // Return the user's input
 
 function getHumanChoice() {
@@ -31,3 +46,9 @@ function getHumanChoice() {
 
 let computerScore = 0;
 let humanScore = 0;
+
+
+
+// Write the logic to play a single round.
+// Your game will be played round by round. You will write a function that takes the human and computer player choices 
+// as arguments, plays a single round, increments the round winner’s score and logs a winner announcement.
